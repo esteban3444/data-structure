229d52ee62f572b58c111076234774803b8ec2dc
esteban3444 <100823974+esteban3444@users.noreply.github.com>
2022-03-05 15:12:11 +0900

    Update class11.java

--- class11.java
+++ class11.java
@@ -1,9 +1,9 @@
 
-public class class11 {
+public class class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("fe");
+		System.out.println("학년과 한번을 입력하시오");
 	}
 
 }
